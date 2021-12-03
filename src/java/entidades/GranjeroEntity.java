e2beee298e60b198e4d4754dc45c7af7eff35f46
Idorma <69322475+Idorma@users.noreply.github.com>
2021-12-03 12:28:52 +0100

    Entidad con anotaciones

--- src/java/entidades/GranjeroEntity.java
+++ src/java/entidades/GranjeroEntity.java
@@ -23,7 +23,6 @@ import javax.persistence.Table;
 public class GranjeroEntity extends UserEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Column
     @OneToMany(cascade=ALL, mappedBy="granjero")
     private List<GranjaEntity> granjas;
     
